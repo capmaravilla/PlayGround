5e28ee96809a6430d14fa253e252d5d2014d3629
David Moriez <capmaravilla@gmail.com>
2023-07-04 14:59:05 +0200

    manual

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-function buscaPrimos(a, b) {
-  let resultPrimos = [];
-  for (let i = a; i <= b; i++) {
-    let primo;
-    if (i <= 1) {
-      primo = false;
-    } else if (i == 2 || i == 3) {
-      primo = true;
-    } else {
-      for (let it = 2; it <= i / 2; it++) {
-        if (i % it === 0) {
-          primo = false;
-        } else {
-          primo = true;
-        }
-      }
-    }
-    if (primo === true) {
-      resultPrimos.push(i);
-    }
-  }
-  console.log(resultPrimos);
-  console.log(`Hay un total de ${resultPrimos.length}`);
+let degrees = 0;
+let isFreezing = false;
+if (degrees >= 0) {
+  isFreezing = true;
 }
-
-buscaPrimos(1, 100);
+console.log('hace frio' + isFreezing);
